51259ec7f0517090f5f3109a64fca367901f5434
darrylbalderas <drrylbalderas@gmail.com>
2018-12-20 08:48:38 -0800

    FIX: Travis pytest issue
    Signed-off-by: darrylbalderas <drrylbalderas@gmail.com>

---
+++ test/test_game.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestGame(unittest.TestCase):
+    def set_up(self):
+        pass
+
+    def test_game_class(self):
+        assert True
+
+
+if __name__ == "__main__":
+    unittest.main()
