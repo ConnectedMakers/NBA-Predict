b3d99132e3a1e8b8143741cec50f87484fde66c1
darrylbalderas <drrylbalderas@gmail.com>
2018-12-20 22:40:47 -0800

    ADD: Getting nba vistor team scoreboard stat
    Signed-off-by: darrylbalderas <drrylbalderas@gmail.com>

--- src/app.py
+++ src/app.py
@@ -1,4 +1,29 @@
 import requests
-url = "http://data.nba.net/10s/prod/v1/20181219/scoreboard.json"
+import json
+from datetime import datetime
+from team import Team
+
+
+def get_current_date():
+    return datetime.today().strftime('%Y%m%d')
+
+
+def get_vistor_team_info(game_payload):
+    team_payload = game_payload['vTeam']
+    tri_code = team_payload['triCode']
+    team_id = team_payload['teamId']
+    wins = team_payload['win']
+    losses = team_payload['loss']
+    linescore = team_payload['linescore']
+    return Team(tri_code, team_id, wins, losses, linescore)
+
+
+date = get_current_date()
+
+url = "http://data.nba.net/10s/prod/v1/{}/scoreboard.json".format(date)
 scoreboard_payload = requests.get(url)
-print(scoreboard_payload.json())
+scoreboard_json = json.loads(scoreboard_payload.text)
+
+games = scoreboard_json['games']
+
+print(get_vistor_team_info(games[0]))
--- src/team.py
+++ src/team.py
@@ -1,13 +1,13 @@
 class Team:
     def __init__(self, name: str, team_id: str, wins: int,
-                 losses: int, line_score: list):
+                 losses: int, line_scores: list):
         self.name = name
         self.team_id = team_id
         self.wins = wins
         self.losses = losses
-        self.line_score = line_score
+        self.line_scores = line_scores
 
     def __str__(self):
-        s = "Team(name={}, team_id={}, wins={}, losses={}, line_score={})"
+        s = "Team(name={}, team_id={}, wins={}, losses={}, line_scores={})"
         return s.format(self.name, self.team_id,
-                        self.wins, self.losses, self.line_score)
+                        self.wins, self.losses, self.line_scores)
